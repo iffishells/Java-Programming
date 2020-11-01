02652dec286a0ace57cc13c52cbc453ad70656e8
iffishells <iffishells@gmail.com>
2020-11-01 22:49:31 +0500

    class and object

--- Hello-world/MyClass.java
+++ Hello-world/MyClass.java
@@ -1,29 +1,11 @@
-import java.util.jar.Attributes.Name;
-
-import javax.swing.text.StyledEditorKit;
-
-import jdk.nashorn.internal.ir.ReturnNode;
-
 public class MyClass{
     public static void main(String[] args){
-       
-        
-        int num1 =  plusMethod(2, 3);
-        System.out.println( "Num1 : "+ num1);
-        double num2  = plusMethod(3.4, 8.7);
-        System.out.println(num2);
     
+    OtherClass myobj = new OtherClass();
+    int num = myobj.x;
 
+    System.out.println("Num : "+ num);
+    
+    
     }
-
-    static int plusMethod(int x , int y){
-
-        return x+y;
-    }
-    // according to the parameter function is calling
-    static double plusMethod(double x , double y){
-
-        return x +y;
-    }
-
 }
