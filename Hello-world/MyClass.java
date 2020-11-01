52617e2537ebdbc2d34141b375f44332284d59fe
iffishells <iffishells@gmail.com>
2020-11-01 14:38:41 +0500

    Variable

--- Hello-world/MyClass.java
+++ Hello-world/MyClass.java
@@ -1,6 +1,17 @@
+import javax.swing.text.StyledEditorKit;
 
-public class MyClass {
+public class MyClass{
     public static void main(String[] args){
-     System.out.println("Hellow world");   
+        System.out.println("Hellow world");
+        String name = "zain";
+
+        System.out.println("Hello " + name);
+        
+        int number = 23;
+
+        float float_Val = 20;
+        System.out.print(float_Val);
+        System.out.println(number);
     }
+
 }
