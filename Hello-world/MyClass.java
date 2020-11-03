3401edf26fdd1883a02ca3d5ce35d5a3753d3c4c
iffishells <iffishells@gmail.com>
2020-11-03 10:11:49 +0500

    updated

--- Hello-world/MyClass.java
+++ Hello-world/MyClass.java
@@ -1,11 +1,12 @@
-public class MyClass{
-    public static void main(String[] args){
-    
-    OtherClass myobj = new OtherClass();
-    int num = myobj.x;
+import jdk.nashorn.api.tree.ForInLoopTree;
 
-    System.out.println("Num : "+ num);
-    
-    
+public class MyClass{
+
+    //
+    public static void main(String[] args){
+        
+        for(int i=0 ; i<10000000000000000 ; i++){
+            System.out.println("Hellow world" + i+4343+434334);
+        }
     }
 }
